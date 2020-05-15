298c1efa865cf5ec062492550ffafcbb4d6cf490
dummyun <51436502+dummyun@users.noreply.github.com>
2020-05-16 02:05:55 +0300

    ++

--- permutations.py
+++ permutations.py
@@ -1,4 +1,4 @@
-ddef permutes(s, n):
+ddef permutes(s, n = len(n)):
     for i in range(n):
         if (n > 1):
             for s in permutes(s, n - 1):
@@ -11,3 +11,14 @@ ddef permutes(s, n):
 
 for s in permutations([1,2,3,4],4):
     print(s)
+    
+    
+def perm(a, k=0):
+    if k == len(a):
+        yield a
+    else:
+        for i in range(k, len(a)):
+            a[k], a[i] = a[i], a[k]
+            for s in perm(a, k + 1):
+                yield s
+            a[k], a[i] = a[i], a[k]
